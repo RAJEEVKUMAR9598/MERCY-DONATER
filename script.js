5853424c5ce360098a4e77058bc1c12b7ed0e893
RAJEEV GUPTA <99285150+RAJEEVKUMAR9598@users.noreply.github.com>
2024-05-07 15:18:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
